6769456aa0178f7fea42e06e162bd5b24c5327dd
manoj-mj2001 <108316542+manoj-mj2001@users.noreply.github.com>
2024-01-28 16:15:12 +0530

    task 4 done

--- PRODIGY_WD_04_PERSONAL-PORTFOLIO-WEBSITE/script.js
+++ PRODIGY_WD_04_PERSONAL-PORTFOLIO-WEBSITE/script.js
@@ -27,12 +27,6 @@ $(document).ready(function () {
     $('html').css('scrollBehavior', 'smooth');
   });
 
-  // toggle menu/navbar script
-  $('.menu-btn').click(function () {
-    $('.navbar .menu').toggleClass('active');
-    $('.menu-btn i').toggleClass('active');
-  });
-
   // typing text animation script
   var typed = new Typed('.typing', {
     strings: ['Front-end Developer&#128187;'],
@@ -46,29 +40,6 @@ $(document).ready(function () {
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
-  });
-
-  // owl carousel script
-  $('.carousel').owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplay: true,
-    autoplayTimeOut: 2000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: false,
-      },
-      600: {
-        items: 2,
-        nav: false,
-      },
-      1000: {
-        items: 3,
-        nav: false,
-      },
-    },
   });
 });
 
